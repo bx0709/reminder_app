d7165cb145a53864060e80314417393e0ed5b376
Daksh Bhatia <daksh001b@gmail.com>
2020-07-26 17:33:51 +0530

    Added swipe to delete functionality. Made a bottom tab for active and completed reminders.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'Pages/HomePage.dart';
+import 'Pages/Reminders.dart';
 
 void main() {runApp(MaterialApp(
-  home: ReminderApp(),
+  routes: {
+    '/': (context) => Reminders(),
+  },
 ));
 }
 
