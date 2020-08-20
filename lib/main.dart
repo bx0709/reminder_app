28a6835d188a0e7f1ddbebb90e6fa96150e686ac
lazy-af-7 <honey.batra007@icloud.com>
2020-08-20 23:22:07 +0530

    anonymus auth main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:reminder_app/Functionality/auth.dart';
 import 'package:reminder_app/Models/user.dart';
 import 'Pages/Reminders.dart';
 import 'Pages/Add_Reminder.dart';
+import 'Models/user.dart';
 
-void main() {
-  runApp(MaterialApp(
-      routes: {
-        '/': (context) => Reminders(),
-        '/add_reminder': (context) => Add_Reminder(),
-      },
-    ),
-  );
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        routes: {
+          '/': (context) => Reminders(),
+          '/add_reminder': (context) => Add_Reminder(),
+        },
+      ),
+    );
+  }
 }
