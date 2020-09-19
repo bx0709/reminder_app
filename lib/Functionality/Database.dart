180346c3dd5d88353992019152d74339d5822800
Daksh Bhatia <daksh001b@gmail.com>
2020-09-19 16:18:48 +0530

    Delete reminder and completed reminders done.

--- lib/Functionality/Database.dart
+++ lib/Functionality/Database.dart
@@ -13,20 +13,22 @@ class DatabaseService {
 
   // Adding new data
   Future addData({String title, String notes, DateTime dateTime,bool allDay, bool email, bool isCompleted}) async{
-    return userCollection.document().setData({
+
+    DocumentReference doc = userCollection.document();
+    return doc.setData({
       'title': title,
       'notes': notes,
       'dateTime': dateTime,
       'allDay': allDay,
       'email': email,
-      'reminderUid': userCollection.document().documentID,
+      'reminderUid': doc.documentID,
       'isCompleted': isCompleted,
     });
   }
 
   //complete reminder
   Future completeReminder(Reminder r){
-    return userCollection.document(r.reminderUid).setData({'isCompleted' : true});
+    return userCollection.document(r.reminderUid).updateData({'isCompleted' : true});
   }
 
   // Deleting data
@@ -45,7 +47,7 @@ class DatabaseService {
       return Reminder(
         title: doc.data['title'] ?? '',
         notes: doc.data['notes'] ?? '',
-        dateTime: doc.data['dateTime'].toDate() ?? null,
+        dateTime: doc.data['dateTime'] != null ? doc.data['dateTime'].toDate() : null,
         allDay: doc.data['allDay'] ?? false,
         email: doc.data['email'] ?? false,
         reminderUid: doc.data['reminderUid'] ?? '',
